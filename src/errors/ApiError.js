2ac086a03f8063d5a572924dc06183c3c95c4df7
Oleh Kaliuzhnyi <el_commandor@outlook.com>
2023-01-20 15:02:59 +0200

    feat: add custom errors

---
+++ src/errors/ApiError.js
@@ -0,0 +1,20 @@
+import { HTTPCodes } from '../router/router.js';
+
+class ApiError extends Error {
+  constructor(message, status = HTTPCodes.BAD_REQUEST) {
+    super(message);
+    this.message = message;
+    this.status = status;
+  }
+}
+
+export const ErrorMessages = {
+  ID_NOT_EXISTS: 'The record with id not exists',
+  NO_PARAMS: 'URL pameters expected',
+  WRONG_ID_FORMAT: 'Wrong id format',
+  NOT_WALID_DATA_FORMAT: 'Not walid data format',
+  INVALUD_URL: 'Invalid URL',
+  METHOD_NOT_SUPPORTED: 'Method not supported'
+}
+
+export default ApiError;
